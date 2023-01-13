c141572fd7841f21036c03fce04759cb08c04949
Grigory <grishka5556@gmail.com>
2023-01-13 17:05:37 +0300

    pricing block

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/UI/Hero";
 import Exercises from "./components/UI/Exercises";
+import Start from "./components/UI/Start";
 
 function App() {
   return <>
     <Header/>
     <Hero/>
     <Exercises/>
+    <Start/>
   </>;
 }
 
--- src/components/UI/Start.jsx
+++ src/components/UI/Start.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../../styles/start.css'
+import trainer from '../../assets/img/trainer.png'
 
 const Start = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div className="container">
+        <div className="start__wrapper">
+          <div className="start__img">
+            <img src={trainer} alt=""/>
+          </div>
+
+          <div className="start__content">
+            <h2 className="section__title">
+              Ready to make a
+              <span className="higlights"> change?</span>
+            </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Deleniti facere fugiat nesciunt nobis possimus quod!</p>
+            <button className="register__btn">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
