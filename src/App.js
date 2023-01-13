869b9f441e711fd64d5db5edb9ba1c151ee5e193
Grigory <grishka5556@gmail.com>
2023-01-13 18:31:59 +0300

    footer block

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Exercises from "./components/UI/Exercises";
 import Start from "./components/UI/Start";
 import Pricing from "./components/UI/Pricing";
 import Testimonials from "./components/UI/Testimonials";
+import Footer from "./components/UI/Footer";
 
 function App() {
   return <>
@@ -14,6 +15,7 @@ function App() {
     <Start/>
     <Pricing/>
     <Testimonials/>
+    <Footer/>
   </>;
 }
 
--- src/components/UI/Testimonials.jsx
+++ src/components/UI/Testimonials.jsx
@@ -1,12 +1,75 @@
 import React from 'react';
 import '../../styles/testimonials.css'
+import {Swiper, SwiperSlide} from "swiper/react";
 
-const Testimonials = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import required modules
+import {EffectCards} from "swiper";
+import avatar01 from '../../assets/img/avatar01.png'
+import avatar02 from '../../assets/img/avatar02.png'
+
+const BASE_URL =
+  "https://sensical-platform.imgix.net/production/logos/wetransfer_gui_machado_experimental_paradigm-2-tif_2022-05-06_1823/";
+
+export default function Testimonials() {
   return (
-   <section>
+    <>
+      <section>
+        <div className="container sliders">
 
-   </section>
+          <h2 className="section__title">Testimonials</h2>
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="mySwiper"
+            loop={true}
+          >
+            <SwiperSlide>
+              <div className="slide__item">
+                <div className="slide__img-01">
+                  <img src={avatar01} alt=""/>
+                </div>
+                <h4>Jesica Fernandes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aut doloremque magni quo sed veritatis. Accusantium doloremque
+                  ducimus eaque et facere ipsam maiores maxime minus nostrum
+                  placeat, quasi, sunt suscipit, vitae.</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="slide__item">
+                <div className="slide__img-02">
+                  <img src={avatar02} alt=""/>
+                </div>
+                <h4>Jesica Fernandes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aut doloremque magni quo sed veritatis. Accusantium doloremque
+                  ducimus eaque et facere ipsam maiores maxime minus nostrum
+                  placeat, quasi, sunt suscipit, vitae.</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="slide__item">
+                <div className="slide__img-03">
+                  <img src={avatar01} alt=""/>
+                </div>
+                <h4>Jesica Fernandes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aut doloremque magni quo sed veritatis. Accusantium doloremque
+                  ducimus eaque et facere ipsam maiores maxime minus nostrum
+                  placeat, quasi, sunt suscipit, vitae.</p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+    </>
   );
-};
+}
 
-export default Testimonials;
+
+
